bc02868033e831e1a9dc88829c697e297bfd7be3
GABRIEL GONCALVES GUEDES <146038835+gguedes00@users.noreply.github.com>
2025-01-21 10:29:47 -0300

    Add files via upload
    Desafio 5

---
+++ desafio_5.py
@@ -0,0 +1,9 @@
+def inverter_string(string):
+    invertida = ""
+    for i in range(len(string) - 1, -1, -1):
+        invertida += string[i]
+    return invertida
+
+
+string = input("Digite uma string: ")
+print(f"String invertida: {inverter_string(string)}")
